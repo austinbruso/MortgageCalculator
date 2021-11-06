90b5dc8f38f1a1c59e969ba51100ec27bfc8506b
austinbruso <55450909+austinbruso@users.noreply.github.com>
2021-11-06 08:01:10 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -1,13 +1,45 @@
-// Get Values
+const { totalmem } = require("os");
 
+// Get Values
 function getValues() {
     // Get input Values
-    let loanAmount = document.getElementById("loanAmount").value;
-    let termLength = document.getElementById("termLength").value;
-    let interestRate = document.getElementById("interestRate").value;
+     let loanAmount = document.getElementById("loanAmount").value;
+     let termLength = document.getElementById("termLength").value;
+     let interestRate = document.getElementById("interestRate").value;
 
     // Turn strings into Integers
     loanAmount = parseInt(loanAmount);
     termLength = parseInt(termLength);
     interestRate = parseInt(interestRate);
+
+    // Validate integers
+    if(Number.isInteger(loanAmount) && Number.isInteger(termLength) && Number.isInteger(interestRate)) {
+        tableLogic(loanAmount, termLength, interestRate);
+            
+
+    } else {
+        alert("Please type an Integer Value only...")
+    }
+}
+
+function tableLogic(balance, term, rate) {
+
+    let array = [];
+
+    for (let index = 1; index <= term; index++) {
+        let tMonthlyPayment =  (balance) * (rate/1200) / (1 - (1 + rate/1200)** (-term)); // $471 - don't change
+        let fremainingBalance = balance;
+    
+       
+
+       array.push(fremainingBalance);
+    }
+
+    return array;
+}
+  
+
+// Display Remaining Data
+function displayData() {
+
 }
